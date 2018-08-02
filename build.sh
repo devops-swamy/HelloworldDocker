0c66c35f4f6535a7c070e97bbf2eb7921108ffb8
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-02 11:12:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp Dockerfile /opt/extract
-cd /opt/extract
-docker build -t tocat .
-docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
-docker push kuruvasomasekhar/tomcat:latest
+sudo cp Dockerfile /opt/extract
+sudo cd /opt/extract
+sudo docker build -t tocat .
+sudo docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
+sudo docker push kuruvasomasekhar/tomcat:latest
