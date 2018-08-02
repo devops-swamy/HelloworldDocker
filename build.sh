8f31286696c6c492fb0095d947fb165cad2a31e1
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-02 11:10:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker build -t helloworld:1.0 .
-docker tag helloworld:1.0 roop1/helloworld:1.0 
-docker push roop1/helloworld:1.0 
+cp Dockerfile /opt/extract
+cd /opt/extract
+docker build -t tocat .
+docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
+docker push kuruvasomasekhar/tomcat:latest
