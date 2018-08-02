74bde8f319641ee8bda872438e1fe0327c800f34
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-02 11:19:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker service create --name myhello --replicas=5 roop1/helloworld:1.0
+docker service create --name tomcat -p 8888:8080 kuruvasomasekhar/tomcat:latest
 
 docker service ls
