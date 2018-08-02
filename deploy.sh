1826d570461fab5351d7ed5f7c8a927a6fa18c81
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-02 11:47:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ exists=$(docker service ls | grep tomcat)
 if [ ! "$exists" ]; then
     docker service create --name tomcat -p 8888:8080 kuruvasomasekhar/tomcat:latest
     else
-    docker service update tomcat:latest
+    docker service update tomcat
 fi
