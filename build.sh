4fab0ce0ce0e0be7612a7fe72c20f27404cf75dc
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-02 11:15:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo cp Dockerfile /opt/extract
 sudo cd /opt/extract
+pwd
 sudo docker build -t tocat .
 sudo docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
 sudo docker push kuruvasomasekhar/tomcat:latest
