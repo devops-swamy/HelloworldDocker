2a9587e3b34f6f2ce677f5520d572b2978ec8fe6
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2018-08-03 10:59:11 +0530

    Updated docker image name

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 sudo cp Dockerfile /opt/extract
 sudo cd /opt/extract
 pwd
-sudo docker build -t tocat /opt/extract
+sudo docker build -t tomcat /opt/extract
 sudo docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
 sudo docker push kuruvasomasekhar/tomcat:latest
