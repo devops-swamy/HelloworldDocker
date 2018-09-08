f18ccc80111e12a44eed8675c3ec3623f9eec1b6
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-09-08 11:23:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ sudo cp Dockerfile /opt/extract
 sudo cd /opt/extract
 pwd
 sudo docker build -t tomcat /opt/extract
-sudo docker tag tomcat:latest kuruvasomasekhar/tomcat:latest 
-sudo docker push kuruvasomasekhar/tomcat:latest
+sudo docker tag tomcat:latest kusupudiswamy/tomcat:latest 
+sudo docker push kusupudiswamy/tomcat:latest
